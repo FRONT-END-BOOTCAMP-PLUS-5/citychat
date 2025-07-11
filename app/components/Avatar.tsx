97e84a7952e179f7475007cf435d568144e9f630
DevYsh <ysh940129@gmail.com>
2025-07-11 11:53:05 +0900

    feat/#37-아바타 컴포넌트 배경색 랜덤 색상변경

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,7 +1,7 @@
 // components/Avatar.tsx
 import React from "react";
 import styles from "./avatar.module.css";
-// Avatar 컴포넌트가 받을 props의 타입을 정의해 줄게!
+// Avatar 컴포넌트가 받을 props의 타입을 정의
 interface AvatarProps {
   name?: string; // 사용자 이름
   size?: number; // 아바타 크기
@@ -11,20 +11,26 @@ interface AvatarProps {
 
 const Avatar: React.FC<AvatarProps> = ({
   name,
-  size = 30, // 기본 크기는 30px
-  backgroundColor = "#ccc",
+  size = 30,
   textColor = "#fff",
 }) => {
   // 이름에서 첫 글자를 추출
   const initial = name ? name.charAt(0).toUpperCase() : "";
 
+  // 배경 색 목록 배열
+  const avatarColors = ["#8ECAE6", "#219EBC", "#023047", "#FFB703", "FB8500"];
+
+  // 배경 색 랜덤
+  const randomBackgroundColor =
+    avatarColors[Math.floor(Math.random() * avatarColors.length)];
+
   return (
     <div
-      className={styles.avatarContainer} //
+      className={styles.avatarContainer}
       style={{
         width: size,
         height: size,
-        backgroundColor: backgroundColor,
+        backgroundColor: randomBackgroundColor,
         color: textColor,
         fontSize: size / 2,
       }}
